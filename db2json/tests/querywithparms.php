4b9542aafcd697b7bcdf9de20756b436e077e65e
Ranger <adc@us.ibm.com>
2017-04-26 17:08:48 -0500

    db2json web test with ?,? parms

---
+++ db2json/tests/querywithparms.php
@@ -0,0 +1,24 @@
+<?php
+$url = getenv("SQL_HOST400");                                                                 
+$db = getenv("SQL_DB400");                                                                 
+$uid = getenv("SQL_UID400");                                                                 
+$pwd = getenv("SQL_PWD400");
+if (!$url) {
+  $url = "http://ut28p63/db2/db2json.pgm";
+}
+$postdata = http_build_query(
+array (
+"json" => "{\"query\":\"select * from QIWS/QCUSTCDT where LSTNAM=? or LSTNAM=?\",\"parm\":[\"Jones\",\"Vine\"],\"fetch\":\"*ALL\"}"
+)
+);
+$context = stream_context_create(
+array(
+    'http' => array(
+        'header'  => "Authorization: Basic " . base64_encode("$uid:$pwd"),
+        'method'  => 'POST',
+        'content' => $postdata
+    )
+));
+$ret = file_get_contents($url, false, $context);
+var_dump($ret);
+?>
