80788b27fcca701fff7ae6c90c7a7f0243419059
Ranger <adc@us.ibm.com>
2017-04-27 11:15:54 -0500

    correct php working with json

--- db2json/tests/query.php
+++ db2json/tests/query.php
@@ -1,24 +1,30 @@
 <?php
-$url = getenv("SQL_HOST400");                                                                 
-$db = getenv("SQL_DB400");                                                                 
-$uid = getenv("SQL_UID400");                                                                 
-$pwd = getenv("SQL_PWD400");
-if (!$url) {
-  $url = "http://ut28p63/db2/db2json.pgm";
-}
-$postdata = http_build_query(
-array (
-"json" => "{\"query\":\"select * from QIWS/QCUSTCDT where LSTNAM='Jones' or LSTNAM='Vine'\",\"fetch\":\"*ALL\"}"
-)
+require "connection.inc";
+$json = json_encode(
+  array(
+    "query" => "select * from QIWS.QCUSTCDT where LSTNAM='Jones' or LSTNAM='Vine'",
+    "fetch" => "*ALL"
+  )
 );
 $context = stream_context_create(
-array(
+  array(
     'http' => array(
-        'header'  => "Authorization: Basic " . base64_encode("$uid:$pwd"),
-        'method'  => 'POST',
-        'content' => $postdata
+       'method'  => 'POST',
+       'header'  => 
+          "Content-Type: Content-Type: application/json\r\n".
+          "Authorization: Basic " . base64_encode("$uid:$pwd"),
+       'content' => $json
     )
-));
+  )
+);
+echo "url:$url\nrequest:$json\n";
 $ret = file_get_contents($url, false, $context);
-var_dump($ret);
+echo "response:\n";
+if ($ret) {
+  $result = json_decode($ret);
+  var_dump($result);
+} else {
+  echo "Failed:\n";
+  var_dump($ret);
+}
 ?>
